6f1eea887c33a4265aafff3e936b20467fbb57e9
Ivy O'Neal-Odom <ivyoo@protonmail.com>
2024-01-30 13:10:02 -0600

    It works! But needs to be cleaned up

--- quad_tree_image_compression.cpp
+++ quad_tree_image_compression.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <opencv2/opencv.hpp>
 
+//Some images used from NASA, the Smithsonian Institute, or Pixabay
+
 class QuadNode {
 	public:
 		//Properties
@@ -25,8 +27,10 @@ class QuadNode {
 
 		void paint_color(){
 
-			std::cout << std::endl;
-			std::cout << "Painting Color" << std::endl;
+			// std::cout << std::endl;
+			// std::cout << "Painting Color" << std::endl;
+			// std::cout << &entropy << std::endl;
+			entropy = 0;
 			// std::cout << rect << std::endl;
 			double count = 0;
 			cv::Vec3d average = cv::Vec3d(0, 0, 0);
@@ -40,33 +44,95 @@ class QuadNode {
 			average /= count;
 			color = cv::Vec3b(average);
 
-			uchar color_len = sqrt(color[0] * color[0] + color[1] * color[1] + color[2] * color[2]);
-			cv::Vec3b norm_color = cv::Vec3b(color[0] / color_len, color[1] / color_len, color[2] / color_len);
+			// std::cout << original_img.at<cv::Vec3b>(300, 900) << std::endl;
+
+			// cv::Vec3b normalized_color = normalize_vec3(color);
+				// cv::Vec3f temp_norm_color = cv::Vec3f((float) color[0] / color_len, (float) color[1] / color_len, (float) color[2] / color_len);
+				// std::cout << color.col(0) << std::endl;
+				// std::cout << color.cols << std::endl;
+				// std::cout << temp_norm_color.col(0) << std::endl;
+				// std::cout << temp_norm_color[0] << std::endl;
+				// std::cout << (int) color[0] << std::endl;
+				// std::cout << (int) color[1] << std::endl;
+				// std::cout << (int) color[2] << std::endl;
+				// std::cout << (float) color[0] << std::endl;
+
+
+
+				// std::cout << std::endl;
+
+				// // normalized_color[0] = temp_norm_color[0];
+				
+				// std::cout << normalized_color.col(0) << std::endl;
+				// std::cout << normalized_color[0] << std::endl;
+				// std::cout << normalized_color[1] << std::endl;
+				// std::cout << normalized_color[2] << std::endl;
+
+				// std::cout << 10 * normalized_color[0] << std::endl;
+
+				// std::cout << std::endl;
+			// }
 			// std::cout << color[0] << ", " << color[1] << ", " << color[2] << std::endl;
 			// std::cout << norm_color[0] << ", " << norm_color[1] << ", " << norm_color[2] << std::endl;
-			// std::cout << color.col(0) << std::endl;
 			// std::cout << colorString() << std::endl;
 			// std::cout << norm_color.col(0) << std::endl;
-			std::cout << sqrt(color[0] * color[0] + color[1] * color[1] + color[2] * color[2]) << std::endl;
+			// std::cout << color_len << std::endl;
+			// std::cout << sqrt(color[0] * color[0] + color[1] * color[1] + color[2] * color[2]) << std::endl;
 
 			//We double dip in this 2d loop. We write to the pixels, and we calculate the entropy
 			for(double x = rect.x; x < rect.x + rect.width; x++){
 				for(double y = rect.y; y < rect.y + rect.height; y++){
 					img.at<cv::Vec3b>(y, x) = color;
 
-					
+					// cv::Vec3f normalized_pixel = normalize_vec3(original_img.at<cv::Vec3b>(y, x));
+					// std::cout << (double)normalized_color.ddot(normalized_pixel) << std::endl;
 					// entropy += 
+
+					// std::cout << std::endl;
+					cv::Vec3b original_pixel = original_img.at<cv::Vec3b>(y, x);
+					// std::cout << (int) original_pixel[0] << std::endl;
+					// std::cout << (int) color[0] << std::endl;
+					// std::cout << abs((int) (original_pixel[0] - color[0])) << std::endl;
+					// std::cout << entropy << std::endl;
+					entropy += abs((int) (original_pixel[0] - color[0]));
+					// std::cout << entropy << std::endl;
+					entropy += abs((int) (original_pixel[1] - color[1]));
+					// std::cout << entropy << std::endl;
+					entropy += abs((int) (original_pixel[2] - color[2]));
+					// std::cout << entropy << std::endl;
+					// entropy += (abs((int) original_pixel[0] - (int) color[0]) + abs((int) original_pixel[1] - (int) color[1]) + abs((int) original_pixel[2] - (int) color[2]));
 				}
 			}
 
-			entropy = rect.width * rect.height + rect.x + rect.y + rand() % 100;
+			// float entropy_scaling = (rect.width * 100);
+			// if(entropy_scaling > 0){
+			// 	entropy /= entropy_scaling;
+			// }else{
+			// 	entropy = -1;
+			// }
+			// entropy = rect.width * rect.height + rect.x + rect.y + rand() % 100;
 			// entropy = rand() % 10;
 
-			std::cout << toString() << std::endl;
+			// std::cout << toString() << std::endl;
 		}
+
+		// cv::Vec3f normalize_vec3(cv::Vec3b vector){
+		// 	float len = sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+		// 	if(len == 0){
+		// 		return cv::Vec3b(0, 0, 0);
+		// 	}
+
+		// 	return cv::Vec3f((float) vector[0] / len, (float) vector[1] / len, (float) vector[2] / len);
+		// }
 
 		//True if this is a leaf node, false if it has children
 		bool is_leaf(){
+			//Edge Case - Child would be 0px in size
+			// if(rect.width / 2 == 0 || rect.height / 2 == 0){
+			// 	entropy = -1;
+			// 	return false;
+			// }
+
 			return children.size() == 0;
 		}
 
@@ -145,7 +211,7 @@ class QuadTree {
 
 		void iterate(int num_iterations){
 			for(int i = 0; i < num_iterations; i++){
-				std::cout << std::endl << std::endl << "Iterating" << std::endl;
+				// std::cout << std::endl << std::endl << "Iterating" << std::endl;
 				//Choose next quad (must be leaf)
 				QuadNode* h_e_n;   //Highest Entropy Node
 				float highest_entropy = -1;
@@ -209,8 +275,10 @@ class QuadTree {
 				// std::cout << std::endl;
 				// root.entropy = 16;
 				// std::cout << root.toString() << std::endl;
-				std::cout << "Highest Entropy Node: " << h_e_n->toString() << std::endl;
-				std::cout << h_e_n->rectString() << std::endl;
+				// std::cout << "Highest Entropy Node: " << h_e_n->toString() << std::endl;
+				// std::cout << h_e_n->rectString() << std::endl;
+				
+
 				h_e_n->add_child(QuadNode(img, original_img, cv::Rect2d(h_e_n->rect.x, h_e_n->rect.y, h_e_n->rect.width / 2, h_e_n->rect.height / 2)));
 				h_e_n->add_child(QuadNode(img, original_img, cv::Rect2d(h_e_n->rect.x + h_e_n->rect.width / 2, h_e_n->rect.y, h_e_n->rect.width / 2, h_e_n->rect.height / 2)));
 				h_e_n->add_child(QuadNode(img, original_img, cv::Rect2d(h_e_n->rect.x, h_e_n->rect.y + h_e_n->rect.height / 2, h_e_n->rect.width / 2, h_e_n->rect.height / 2)));
@@ -266,12 +334,16 @@ class QuadTree {
 };
 
 int main(){
-	cv::Mat img = cv::imread("C:\\Users\\Ivy\\Documents\\Programming\\C++\\Quad Tree Image Compression\\test.jpg", cv::IMREAD_COLOR);
-	cv::resize(img, img, cv::Size(700, 700));	
+	cv::Mat img = cv::imread("C:\\Users\\Ivy\\Documents\\Programming\\C++\\Quad Tree Image Compression\\trees-8512979_1280.jpg", cv::IMREAD_COLOR);
+	// int test_size = 10;
+	// cv::resize(img, img, cv::Size(test_size, test_size));
+	cv::resize(img, img, cv::Size((img.size[1] / 700) * img.size[0], 700));	
 
 	QuadTree quad_tree(img);
-	quad_tree.iterate(0);
+	quad_tree.iterate(1000);
 
+	
+	// cv::resize(img, img, cv::Size(500, 500), 0.0, 0.0, 0);	
 	cv::imshow("", img);
 	int k = cv::waitKey(0);
 	cv::destroyAllWindows();
